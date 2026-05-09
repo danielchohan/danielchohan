56f1db09930b5ff18a6f4f5761aa17b5ebb0ff54
Daniel Chohan <danielchohan18@gmail.com>
2026-05-09 17:44:21 -0400

    Added JS code featuring the stack overflow link i got my js script from.

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,7 @@
+
+/* To fully implement the burger bar I used this
+https://stackoverflow.com/questions/69713895/navigation-menu-to-burger-menu
+*/
 const burger = document.querySelector(".burgerbar");
 const navlinks = document.querySelector(".navlinks");
 
